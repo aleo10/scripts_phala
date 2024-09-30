594b3096b36b6ede7fa2e9f52d8328bb76ae6b42
aleo10 <aleammiraglia@hotmail.com>
2024-09-30 17:00:07 -0300

    Update limite.sh

--- limite.sh
+++ limite.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-# Crear el cgroup
-sudo cgcreate -g cpu:/limited_group
+# Actualizar repositorios e instalar htop y cgroup-tools
+echo "Actualizando repositorios e instalando htop y cgroup-tools..."
+sudo apt update
+sudo apt install -y htop cgroup-tools
 
-# Configurar la limitación de CPU al 50%
+# Limitar los subprocesos de ./app al núcleo 0 usando taskset
+echo "Limitar subprocesos de ./app al núcleo 0 con taskset..."
+ps -eLf | grep "[.]\/app" | awk '{print $4}' | xargs -I {} sudo taskset -p 0x1 {}
+
+# Crear el cgroup y configurar la limitación de CPU al 50%
+echo "Creando el cgroup y limitando el uso de CPU al 50%..."
+sudo cgcreate -g cpu:/limited_group
 echo 50000 | sudo tee /sys/fs/cgroup/cpu/limited_group/cpu.cfs_quota_us
 echo 100000 | sudo tee /sys/fs/cgroup/cpu/limited_group/cpu.cfs_period_us
 
-# Añadir todos los subprocesos de ./app al cgroup
+# Añadir los subprocesos de ./app al cgroup
+echo "Añadiendo subprocesos de ./app al cgroup..."
 for pid in $(ps -eLf | grep "[.]\/app" | awk '{print $4}'); do
     sudo cgclassify -g cpu:/limited_group $pid
 done
 
-echo "Subprocesos de ./app limitados y añadidos al cgroup 'limited_group'."
+echo "Subprocesos de ./app limitados al núcleo 0 y añadidos al cgroup 'limited_group'."
